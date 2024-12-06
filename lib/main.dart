f5ab4e899b1a4c43e7f30a696e57a64fb7f46b5b
Basil <basil.0522002@gmail.com>
2024-12-06 19:36:31 +0200

    added the provider in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:requests_management_system/Features/Login/Presentation/Provider/AuthProvider.dart';
+import 'package:requests_management_system/Features/Profile/Presentation/Provider/profile_provider.dart';
 import 'package:requests_management_system/my_app.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => AuthProvider(),)
-    ],
-    child: const MyApp()));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => AuthProvider(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => ProfileProvider(),
+    ),
+  ], child: const MyApp()));
 }
