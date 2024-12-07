d7ac8c6ba7ecbde22d7334729163783886fd6ad9
Basil <basil.0522002@gmail.com>
2024-12-07 14:22:49 +0200

    added token and employee id in the profile provider to handle get specific profile

--- lib/Features/Profile/Presentation/Provider/profile_provider.dart
+++ lib/Features/Profile/Presentation/Provider/profile_provider.dart
@@ -4,37 +4,15 @@ import 'package:requests_management_system/Features/Profile/Data/ProfileModel.da
 
 class ProfileProvider extends ChangeNotifier {
   ProfileModel? profileModel;
-  Future<void> getProfile() async {
-    profileModel = await ProfileService.getProfile();
-    notifyListeners();
+
+  Future<void> fetchProfile(
+      {required String token, required int employeeId}) async {
+    try {
+      final profileService = ProfileService(token: token);
+      profileModel = await profileService.getProfile(employeeId);
+      notifyListeners();
+    } catch (e) {
+      print("Error fetching profile: $e");
+    }
   }
 }
-
-// class ProfileProvider extends ChangeNotifier {
-//   ProfileModel? profileModel; // Stores profile data
-//   bool isLoading = false; // Tracks loading state
-//   String? errorMessage; // Stores error messages (if any)
-//
-//   /// Fetch profile data
-//   Future<void> getProfile() async {
-//     isLoading = true; // Set loading to true
-//     errorMessage = null; // Reset error message
-//     notifyListeners(); // Notify UI of state change
-//
-//     try {
-//       profileModel = await ProfileService.getProfile(); // Fetch profile
-//     } catch (error) {
-//       errorMessage =
-//           "حدث خطأ أثناء تحميل البيانات. حاول مرة أخرى."; // Handle error
-//       profileModel = null; // Reset profile data
-//     } finally {
-//       isLoading = false; // Stop loading
-//       notifyListeners(); // Notify UI of state change
-//     }
-//   }
-//
-//   /// Refresh profile manually
-//   Future<void> refreshProfile() async {
-//     await getProfile();
-//   }
-// }
