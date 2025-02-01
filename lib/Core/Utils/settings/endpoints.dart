f9e68ae4dd56bab59440c62206d0a7867021ad9e
Basil <basil.0522002@gmail.com>
2025-02-01 23:50:01 +0200

    added new endpoints

--- lib/Core/Utils/settings/endpoints.dart
+++ lib/Core/Utils/settings/endpoints.dart
@@ -1,11 +1,19 @@
 class Endpoints {
-  static const String baseUrl = 'http://requests-management-system.runasp.net/api/';
+  static const String baseUrl =
+      'http://requests-management-system.runasp.net/api/';
 
   static const String login = "Employee/Login";
 
-  static const String getEmployeeTransactions = "Transaction/GetAllTransactionsByEmployeeId/";
+  static const String getEmployeeTransactions =
+      "Transaction/GetAllTransactionsByEmployeeId/";
 
-  static const String getStaffTransactions = "Transaction/GetStaffTransactions/";
+  static const String getStaffTransactions =
+      "Transaction/GetStaffTransactions/";
+
+  static const String sendRequestTransaction = "Transaction/PostTransaction";
+
+  static const String getSubstituteEmployee =
+      "Employee/GetEmployeesByDepartmentName/";
 }
 
 class ApiKey {
